3e8734153c809936d8913b67e0e639a7c2503fbc
AanchalMehra <121108090+AanchalMehra@users.noreply.github.com>
2026-09-14 01:12:58 +0530

    Modify cookie settings in auth controller
    Updated cookie settings to always use secure and empty sameSite.

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -27,8 +27,8 @@ export const signUp= async(req,res)=>{
         const token= await genToken(user._id);
         res.cookie("token",token,{
             httpOnly:true,
-            secure:process.env.NODE_ENV==="production",
-            sameSite:"strict",
+            secure:true,
+            sameSite:"",
             maxAge:7*24*60*60*1000
         })
 
@@ -65,8 +65,8 @@ export const login= async(req,res)=>{
         const token= await genToken(user._id);
         res.cookie("token",token,{
             httpOnly:true,
-            secure:process.env.NODE_ENV==="production",
-            sameSite:"strict",
+            secure:true,
+            sameSite:"",
             maxAge:7*24*60*60*1000
         })
 
